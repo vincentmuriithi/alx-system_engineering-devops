b293e83cff33151b96eedf0bab921dcf48172cea
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-03-15 16:22:40 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-#increase the amount of traffic that an nginx server can handle
+# Increases the amount of traffic an Nginx server can handle.
 
-#Increase the ULIMIT of the default file
-exec {"fix--for--nginx":
-command => 'sed -i "s/15/4096/" /etc/default/nginx/',
-path => '/usr/local/bin:/bin/'
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
-#Restart nginx
-exec {'restart-nginx':
-command => 'nginx restart',
-path => 'etc/init.d/'
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
